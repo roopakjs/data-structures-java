65e1368178a51b7a9adef3fbb101ae7963260559
ROOPAK JS <140954845+roopakjs@users.noreply.github.com>
2026-01-15 20:58:59 +0530

    Implement binary search in BinarySearch.java

---
+++ searching/BinarySearch.java
@@ -0,0 +1,24 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {5, 10, 15, 20, 25, 30};
+        int key = 20;
+        int low = 0, high = arr.length - 1;
+        boolean found = false;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == key) {
+                found = true;
+                break;
+            } else if (arr[mid] < key) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        if (found)
+            System.out.println("Element found");
+        else
+            System.out.println("Element not found");
+    }
+}
